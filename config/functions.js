2a2473dc6175c0d28a3b6fb35a76a2bb7416b1d8
Erfan <erfanhosseinizadeh@gmail.com>
2023-10-30 20:48:39 +0330

    first commit

--- config/functions.js
+++ config/functions.js
@@ -1,12 +1,22 @@
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev ? "http://localhost:3000" : process.env.URL;
 
-export const fetchFromServer = async (url, method, opt) => {
+export const fetchFromServer = async (url, method, userOptions) => {
+  const url = `${server}/api/${url}`;
+
+  const initialOptions = {
+    next: { revalidate: 5 },
+    method: method || "GET",
+    cache: "no-store",
+  };
+
+  const options = {
+    ...initialOptions,
+    ...userOptions,
+  };
+
   try {
-    return await fetch(`${server}/api/${url}`, {
-      ...opt,
-      method: method || "GET",
-    }).then((res) => res.json());
+    return await fetch(url, options).then((res) => res.json());
   } catch (error) {
     console.log(error);
     return JSON.stringify(error);
